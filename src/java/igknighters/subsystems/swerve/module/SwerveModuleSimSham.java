9b75596fe5f70caea0138f494334345d457b0bc5
oh-yes-0-fps <foryt.bowan@gmail.com>
2025-01-31 21:43:16 -0500

    completely rewrite the closed loop class, it is 200 loc shorter now and performs more accurately

--- src/java/igknighters/subsystems/swerve/module/SwerveModuleSimSham.java
+++ src/java/igknighters/subsystems/swerve/module/SwerveModuleSimSham.java
@@ -33,8 +33,8 @@ public class SwerveModuleSimSham extends SwerveModule {
 
   private final ShamMCX driveMotor;
   private final ShamMCX steerMotor;
-  private final ClosedLoop<VoltageUnit, AngularVelocityUnit, AngleUnit> driveLoop;
-  private final ClosedLoop<VoltageUnit, AngleUnit, AngleUnit> steerLoop;
+  private final ClosedLoop<VoltageUnit, AngularVelocityUnit> driveLoop;
+  private final ClosedLoop<VoltageUnit, AngleUnit> steerLoop;
 
   public SwerveModuleSimSham(
       final int moduleId, SimSwerveOdometryThread odoThread, ShamSwerve sim) {
@@ -56,8 +56,7 @@ public class SwerveModuleSimSham extends SwerveModule {
         ClosedLoop.forVoltageAngle(
             PIDFeedback.forAngular(Volts, Rotations, kSteerMotor.kP, kSteerMotor.kD)
                 .withContinuousAngularInput(),
-            SimpleFeedforward.forVoltage(Rotations, kSteerMotor.kS, 0.0, 0.0, sim.timing().dt()),
-            true);
+            SimpleFeedforward.forVoltage(Rotations, kSteerMotor.kS, 0.0, 0.0, sim.timing().dt()));
 
     steerMotor.configSensorToMechanismRatio(kSwerve.STEER_GEAR_RATIO);
 
