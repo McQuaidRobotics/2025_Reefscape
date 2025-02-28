a19409939205fb53f3ff008f40935abffe57ec0d
oh-yes-0-fps <foryt.bowan@gmail.com>
2025-02-28 16:53:29 -0500

    updated elevator accel

--- src/java/igknighters/subsystems/superStructure/SuperStructureConstants.java
+++ src/java/igknighters/subsystems/superStructure/SuperStructureConstants.java
@@ -57,8 +57,8 @@ public class SuperStructureConstants {
     public static final double kV = kKrakenX60Foc.kV * GEAR_RATIO;
     public static final double kA = 0.00;
 
-    public static final double MAX_VELOCITY = (12.0 - kS) / kV;
-    public static final double MAX_ACCELERATION = MAX_VELOCITY / 1.0;
+    public static final double MAX_VELOCITY = (12.0 - kS - kG) / kV;
+    public static final double MAX_ACCELERATION = MAX_VELOCITY / 0.8;
 
     public static final double HOMING_VOLTAGE = -kS - 2.0;
 
