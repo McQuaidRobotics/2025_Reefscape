df6df5dc140ae1bfc4387a85ff9f592ea7513732
Henry Winchester <henryj.winchester@outlook.com>
2025-03-04 20:38:49 -0500

    i just wanna rock

--- src/java/igknighters/subsystems/led/Led.java
+++ src/java/igknighters/subsystems/led/Led.java
@@ -1,48 +1,24 @@
 package igknighters.subsystems.led;
 
-import igknighters.Robot;
-import igknighters.constants.ConstValues.kLed;
 import igknighters.subsystems.Subsystems.SharedSubsystem;
-import igknighters.subsystems.led.LedAnimation.PartialAnimation;
-import igknighters.subsystems.led.driver.Driver;
-
-import igknighters.subsystems.led.driver.SimDriver;
+import igknighters.subsystems.led.driver.PWMDriver;
 import wpilibExt.Tracer;
 
 public class Led implements SharedSubsystem {
-  private final Driver[] drivers;
-
+  //private final Driver[] drivers;
+  public final PWMDriver pwm1;
+  public final PWMDriver pwm2;
   public Led() {
-    // if (Robot.isReal()) {
-    //   // drivers = new Driver[] {
-    //   //   new PWMDriver(60, 9),
-    //   //   new PWMDriver(60, 0)
-    //   };
-    // } else {
-    //   drivers = new Driver[] {new SimDriver()};
-    // }
-    drivers = new Driver[] {new SimDriver()};
+    pwm1 = new PWMDriver(9, 60);
+    pwm2 = new PWMDriver(0, 60);
   }
-  /**
-   * This is the function that is called to animate the LEDs using the animations from the LedAnimations enum (not WPILIB)
-   * @param handle
-   * @param animation
-   */
 
-  public void animate(int driverIndex, LedAnimation animation) {
-    for (Driver driver: drivers) {
-      driver.animate(
-        new PartialAnimation[] {new PartialAnimation(kLed.LED_COUNT, kLed.CANDLE_LEDS, animation)}
-      );
-    }
-  }
 
   @Override
   public void periodic() {
     Tracer.startTrace("LedPeriodic");
-    for(Driver drivey: drivers) {
-      Tracer.traceFunc("DriverPeriodic", drivey::periodic);
-    }
+    pwm1.periodic();
+    pwm2.periodic();
     Tracer.endTrace();
   }
 }
--- src/java/igknighters/subsystems/led/driver/SimDriver.java
+++ src/java/igknighters/subsystems/led/driver/SimDriver.java
@@ -1,10 +1,7 @@
 package igknighters.subsystems.led.driver;
 
-import igknighters.subsystems.led.LedAnimation.PartialAnimation;
 
 public class SimDriver extends Driver {
-  @Override
-  public void animate(PartialAnimation[] animations) {}
   @Override
   public void periodic() {}
 }
