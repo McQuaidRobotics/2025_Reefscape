6919f6cbd531ed47ef0bbf15587a935bd45f9ea3
oh-yes-0-fps <foryt.bowan@gmail.com>
2025-04-08 15:53:59 -0400

    remove LOS for sim tags

--- src/java/igknighters/SimCtx.java
+++ src/java/igknighters/SimCtx.java
@@ -4,12 +4,9 @@ import static edu.wpi.first.units.Units.KilogramSquareMeters;
 import static edu.wpi.first.units.Units.Seconds;
 import static edu.wpi.first.units.Units.Volts;
 
-import edu.wpi.first.apriltag.AprilTag;
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
-import igknighters.constants.AprilTags;
 import igknighters.constants.ConstValues;
 import igknighters.constants.ConstValues.kRobotIntrinsics;
 import igknighters.constants.FieldConstants;
@@ -18,7 +15,6 @@ import igknighters.subsystems.swerve.SwerveConstants.ModuleConstants.kSteerMotor
 import igknighters.subsystems.swerve.SwerveConstants.ModuleConstants.kWheel;
 import igknighters.subsystems.swerve.SwerveConstants.kSwerve;
 import igknighters.util.plumbing.Channel.Receiver;
-import java.util.ArrayList;
 import monologue.GlobalField;
 import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.VisionSystemSim;
@@ -134,22 +130,22 @@ public class SimCtx {
       final Pose2d robotPose = simRobot.getDriveTrain().getChassisWorldPose();
       GlobalField.setObject("SimRobot", robotPose);
 
-      aprilTagSim.clearAprilTags();
-      ArrayList<AprilTag> visibleTags = new ArrayList<>();
-      for (AprilTag tag : AprilTags.TAGS) {
-        boolean los =
-            arena()
-                .lineOfSight(
-                    robot().getDriveTrain().getChassisWorldPose().getTranslation(),
-                    tag.pose.getTranslation().toTranslation2d());
-        if (los) {
-          visibleTags.add(tag);
-        }
-      }
-      AprilTagFieldLayout layout =
-          new AprilTagFieldLayout(
-              visibleTags, FieldConstants.FIELD_LENGTH, FieldConstants.FIELD_WIDTH);
-      aprilTagSim.addAprilTags(layout);
+      // aprilTagSim.clearAprilTags();
+      // ArrayList<AprilTag> visibleTags = new ArrayList<>();
+      // for (AprilTag tag : AprilTags.TAGS) {
+      //   boolean los =
+      //       arena()
+      //           .lineOfSight(
+      //               robot().getDriveTrain().getChassisWorldPose().getTranslation(),
+      //               tag.pose.getTranslation().toTranslation2d());
+      //   if (los) {
+      //     visibleTags.add(tag);
+      //   }
+      // }
+      // AprilTagFieldLayout layout =
+      //     new AprilTagFieldLayout(
+      //         visibleTags, FieldConstants.FIELD_LENGTH, FieldConstants.FIELD_WIDTH);
+      // aprilTagSim.addAprilTags(layout);
       aprilTagSim.update(robotPose);
 
       objectDetectionSim.update(robotPose);
